a3874bd658dc2db6a5d900872b8b24c56d87a613
Aliyat <liyabaealiyat@gmail.com>
2026-05-30 08:26:21 +0100

    testingpage

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,5 @@
 import ComingSoon from "@/components/common/ComingSoon";
 
 export default function SignInPage() {
-  return <ComingSoon title="Sign In Page" />;
+  return <>Aliyat is working</>;
 }
