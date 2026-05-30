e1ee98b4fa66f55c0d04854ce82772e76b46e9bf
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-05-30 09:59:55 +0100

    feat: implement dynamic sidebar active state

--- src/components/layout/Sidebar/SidebarNav.tsx
+++ src/components/layout/Sidebar/SidebarNav.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import SidebarItem from "./SidebarItem";
 import { navigationItems } from "@/constants/navigation";
+import { usePathname } from "next/navigation";
 
 export default function SidebarNav() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex flex-col gap-2">
-      {navigationItems.map((item, index) => (
+      {navigationItems.map((item) => (
         <SidebarItem
           key={item.label}
           label={item.label}
           href={item.href}
           icon={item.icon}
-          active={index === 0}
+          active={pathname === item.href}
         />
       ))}
     </nav>
