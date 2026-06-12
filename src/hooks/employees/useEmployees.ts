7db4d6ee0b622f5cdb6c3efa65c74b3e031487f8
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-06-13 00:15:06 +0100

    fix: applied suggested fixes

--- src/hooks/employees/useEmployees.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getEmployees } from "@/services/employee.service";
-
-export const useEmployees = () => {
-  return useQuery({
-    queryKey: ["employees"],
-    queryFn: getEmployees,
-  });
-};
